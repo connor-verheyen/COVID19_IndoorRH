964e461b5c6248e28453fb4823c13b0c9570cfe5
connor-verheyen <68815194+connor-verheyen@users.noreply.github.com>
2020-10-29 09:03:02 -0400

    Update and rename make_lags.py to create_timelagged_dataframe.py

--- useful_functions_and_scripts/create_timelagged_dataframe.py
+++ useful_functions_and_scripts/create_timelagged_dataframe.py
@@ -11,3 +11,17 @@ def make_lags(dataframe,merging_var,variable,num_lags):
     df[variable+'_lag{}'.format(i)] = df.groupby('Country')[variable].shift(i) # Create all of the lags of the given input variable 
 
   return df
+
+# Function to create a dataframe with lagged versions of the variable and the actual outbreak metrics 
+
+def lagged_var_df(dataframe,merging_var,variable,num_lags,COVIDstats):
+
+  df = dataframe.copy()
+
+  outcomes = df[COVIDstats]
+
+  lagged_variables = make_lags(dataframe,merging_var,variable,num_lags)
+
+  merged = lagged_variables.merge(right=outcomes,how='left',on=['Country',merging_var])
+
+  return merged
